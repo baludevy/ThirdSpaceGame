daccd59b611ce9b4596a92243a63061c79b0aa91
Just_Paprika <kostyal.peter123@gmail.com>
2026-09-19 16:37:25 +0200

    stacking and saving added

--- Assets/Scripts/ItemDragHandler.cs
+++ Assets/Scripts/ItemDragHandler.cs
@@ -1,5 +1,7 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -29,32 +31,67 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1f;
+
         Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
-        if(dropSlot == null)
+        if (dropSlot == null)
         {
             GameObject item = eventData.pointerEnter;
-            if(item != null)
+            if (item != null && eventData.pointerEnter != null)
             {
-                dropSlot = item.GetComponentInParent<Slot>();
+                dropSlot = eventData.pointerEnter.GetComponentInParent<Slot>();
             }
         }
         Slot originalSlot = originalParent.GetComponent<Slot>();
 
         if (dropSlot != null)
         {
+            Item draggedItem = GetComponent<Item>();
             if (dropSlot.currentItem != null)
             {
-                dropSlot.currentItem.transform.SetParent(originalParent.transform);
-                originalSlot.currentItem = dropSlot.currentItem;
-                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                Item targetItem = dropSlot.currentItem.GetComponent<Item>();
+                if (draggedItem != null && targetItem != null && draggedItem.ID == targetItem.ID)
+                {
+                    if (
+                        draggedItem != null
+                        && targetItem != null
+                        && draggedItem.ID == targetItem.ID
+                    )
+                    {
+                        targetItem.Quantity += draggedItem.Quantity;
+                        targetItem.UpdateQuantityDisplay();
+                    }
+                    if (originalSlot != null)
+                    {
+                        originalSlot.currentItem = null;
+                    }
+
+                    Destroy(gameObject);
+                    return;
+                }
+                else
+                {
+                    dropSlot.currentItem.transform.SetParent(originalParent);
+                    dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition =
+                        Vector2.zero;
+
+                    if (originalSlot != null)
+                    {
+                        originalSlot.currentItem = dropSlot.currentItem;
+                    }
+
+                    transform.SetParent(dropSlot.transform);
+                    dropSlot.currentItem = gameObject;
+                }
             }
             else
             {
-                originalSlot.currentItem = null;
+                if (originalSlot != null)
+                {
+                    originalSlot.currentItem = null;
+                }
+                transform.SetParent(dropSlot.transform);
+                dropSlot.currentItem = gameObject;
             }
-
-            transform.SetParent(dropSlot.transform);
-            dropSlot.currentItem = gameObject;
         }
         else
         {
