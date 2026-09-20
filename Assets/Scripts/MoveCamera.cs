28a908d0420ab6daad643ee088c067e15d7d3748
balu <me@balu.moe>
2026-09-20 13:51:58 +0200

    Add pixel perfect camera

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -1,43 +1,32 @@
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
-{
+public class MoveCamera : MonoBehaviour {
     public Transform target;
     public float smoothTime = 0.12f;
 
     Vector3 velocity = Vector3.zero;
     float cameraZ;
-    
-    public void Start()
-    {
+
+    public Vector3 offset;
+
+    public void Start() {
         cameraZ = transform.position.z;
     }
 
-    void LateUpdate()
-    {
-        if(target == null)
-        {
+    void LateUpdate() {
+        if (target == null) {
             return;
         }
 
-    float playerX = target.position.x;
-    float playerY = target.position.y;
+        float playerX = target.position.x;
+        float playerY = target.position.y;
 
-    Vector3 targetPosition = new Vector3(
-        playerX,
-        playerY,
-        cameraZ 
-    );
-
-    Vector3 currentPosition = transform.position;
-    
-    Vector3 nextposition = Vector3.SmoothDamp(
-        currentPosition,
-        targetPosition,
-        ref velocity,
-        smoothTime
-    );
-
-    transform.position = nextposition;  
+        Vector3 targetPosition = new Vector3(
+            playerX,
+            playerY,
+            cameraZ
+        ) + offset;
+        
+        transform.position = targetPosition;
     }
 }
